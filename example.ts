8dcfba6ce5e8803dafcea1f675f31912c9b3f4b3
Doron Tsur <qballer@gmail.com>
2019-01-01 11:36:04 +0200

    comment

--- example.ts
+++ example.ts
@@ -1,10 +1,11 @@
 const b = getB() 
 
 function getB() {
+    // will throw C is not a constructor in runtime
     return new C()
 }
 
-//Will generate Class 'C' used before its declaration.
-//const instanceOfC = new C()
+// Will generate Class 'C' used before its declaration.
+// const instanceOfC = new C()
 
 class C {}
